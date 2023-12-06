b8adc25ec98e70530f16cf6495510344acb10d58
Draniev <23260952+Draniev@users.noreply.github.com>
2023-12-06 13:37:37 +0300

    add docstring for func in str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,9 @@
 def capitalize_str(string: str) -> str:
+    """
+    Преобразует переданную строку в заглавные буквы
+
+    Parameters:
+    string: str
+        Строка, которую требуется преобразовать
+    """
     return string.capitalize()
